6b5631a0d77f64cf24614fa962ce11dcc00c7d65
syifa2606 <50442078+syifa2606@users.noreply.github.com>
2019-07-27 19:45:03 +0700

    Add files via upload

---
+++ soal2.php
@@ -0,0 +1,17 @@
+<?php 
+	$username ="-Mark24";
+	$password ="codeFuture=,12";
+
+	if (preg_match("/^[^0-9\.\-\_\@\?\!\$\#^\&\*][a-zA-Z0-9]{5,9}$/", $username)) {
+		echo  "Data username valid";
+	} else {
+		echo  "Data username tidak valid";
+	}
+
+
+	if (preg_match("/^([a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[#$^+=!*()@%&]).{8,}$/", $password)) {
+		echo  "<br> Data password valid";
+	} else {
+		echo  "<br> Data password tidak valid";
+	}
+?>
